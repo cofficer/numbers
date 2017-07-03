73b1f6f35b4f361f6051d25532c6315b5db82b4d
cofficer <christoffer.gahnstrom@hotmail.com>
2017-07-03 11:09:13 +0200

    Added the start to interpolation

--- appendEyelinkMEG.m
+++ appendEyelinkMEG.m
@@ -20,11 +20,20 @@ for ieye = 1:length(alleyes)
   fprintf('Eyelink asc %s\n',alleyes(ieye).name)
 
   %load the MEG data
-  dat_megname      = sprintf('%s%s_S%s_P%s.mat',megpath,alleyes(ieye).name(2:3),alleyes(ieye).name(5),alleyes(ieye).name(7))
+  dat_megname      = sprintf('%s%s_S%s_P%s.mat',megpath,alleyes(ieye).name(2:3),alleyes(ieye).name(5),alleyes(ieye).name(7));
   dat_meg          = load(dat_megname)
 
   %Insert the eyelink channels in the MEG data
+  %Consideration: Need to figure out the most appropriate way to combine the two datasets.
+  %Naming: UADC004 already excists, so I will extend the convention to UADC009, but this is not possible unless the lengths are identical. The only solution might be to interpolate the eyelink data. This can be checked against the eyelink data which has been simulaneously collected.
 
+  method = 'linear' %Unsure of the most appropriate interp method.
+  %x, is the sample index. v, are the corresponding values. xq, is the queried points.
+  x = dat_eye.asc.time{1}-301;%Is this reasonable? I can either stretch the existing or ignore the last part...
+  vq = interp1(x,v,xq,method)
+
+
+  %Plot the overlap between the MEG eyelink and the upsampled data.
   %Save the new MEG files
 
 
