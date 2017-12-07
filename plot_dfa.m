75810dd641bf0446eaa1468e258275f97fef79fa
cofficer <christoffer.gahnstrom@hotmail.com>
2017-12-07 16:01:00 +0100

    rerun dfa low id

--- plot_dfa.m
+++ plot_dfa.m
@@ -4,6 +4,7 @@ function T_dfa = plot_dfa(~)
 %Created 7/12-2017
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+clear all
 
 %load csv of session info.
 cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos')
@@ -20,12 +21,12 @@ rest_dfa = dir('P*.mat');
 rest_dfa={rest_dfa.name};
 
 %Variable of DFA exponent
-nocebo_trl      =NaN(1,45);
-nocebo_rest     =NaN(1,45);
-lora_trl        =NaN(1,45);
-lora_rest       =NaN(1,45);
-placebo_trl     =NaN(1,45);
-placebo_rest    =NaN(1,45);
+nocebo_trl      =NaN(1,45)';
+nocebo_rest     =NaN(1,45)';
+lora_trl        =NaN(1,45)';
+lora_rest       =NaN(1,45)';
+placebo_trl     =NaN(1,45)';
+placebo_rest    =NaN(1,45)';
 
 %Loop and store trial-based dfa exponent. Important to correctly average.
 for idfa = 1:length(trl_dfa)
@@ -70,19 +71,14 @@ for idfa = 1:length(trl_dfa)
 end
 
 %store the resting dfa exponent.
-for i2dfa = 1:length(trl_dfa)
+for i2dfa = 1:length(rest_dfa)
   load(rest_dfa{i2dfa})
 
   %Identify the current participant.
-  if strcmp(trl_dfa{idfa}(3),'_')
-    numP        = str2num(trl_dfa{idfa}(2));
-    curr_sess   = str2num(trl_dfa{idfa}(5));
-    curr_block  = str2num(trl_dfa{idfa}(8));
-  else
-    numP = str2num(trl_dfa{idfa}(2:3));
-    curr_sess   = str2num(trl_dfa{idfa}(6));
-    curr_block  = str2num(trl_dfa{idfa}(9));
-  end
+  numP        = str2num(rest_dfa{i2dfa}(2:3));
+  curr_sess   = str2num(rest_dfa{i2dfa}(6));
+  curr_block  = str2num(rest_dfa{i2dfa}(9));
+
 
   curr_order = drug_order(numP,2:3);
   sess_pla   = curr_order(1)==curr_sess;
@@ -90,26 +86,73 @@ for i2dfa = 1:length(trl_dfa)
 
   %Crete case of nocebo, trl
   if curr_sess==1
-    if ~isnan(nocebo_trl(numP))
-      nocebo_trl(numP)=mean([mean(dfa_all{3}.exp),nocebo_trl(numP)]);
+    if ~isnan(nocebo_rest(numP))
+      nocebo_rest(numP)=mean([mean(dfa_all{3}.exp),nocebo_rest(numP)]);
     else
-      nocebo_trl(numP)  = mean(dfa_all{3}.exp);
+      nocebo_rest(numP)  = mean(dfa_all{3}.exp);
     end
-  %Crete case of placebo, trl
+  %Crete case of placebo, rest
   elseif sess_pla
-    if ~isnan(placebo_trl(numP))
-      placebo_trl(numP)=mean([mean(dfa_all{3}.exp),placebo_trl(numP)]);
+    if ~isnan(placebo_rest(numP))
+      placebo_rest(numP)=mean([mean(dfa_all{3}.exp),placebo_rest(numP)]);
     else
-      placebo_trl(numP)  = mean(dfa_all{3}.exp);
+      placebo_rest(numP)  = mean(dfa_all{3}.exp);
     end
   elseif sess_drug
-    if ~isnan(lora_trl(numP))
-      lora_trl(numP)=mean([mean(dfa_all{3}.exp),lora_trl(numP)]);
+    if ~isnan(lora_rest(numP))
+      lora_rest(numP)=mean([mean(dfa_all{3}.exp),lora_rest(numP)]);
     else
-      lora_trl(numP)  = mean(dfa_all{3}.exp);
+      lora_rest(numP)  = mean(dfa_all{3}.exp);
     end
   end
 end
+
+%Create table
+
+T_dfa = table(nocebo_trl,nocebo_rest,lora_trl,lora_rest,placebo_trl,placebo_rest);
+sum_dfa = summary(T_dfa);
+
+idx_complete=isnan(T_dfa{:,{'nocebo_trl','nocebo_rest','lora_trl','lora_rest','placebo_trl','placebo_rest'}});
+
+%The ones are complete participants.
+idx_complete=sum(idx_complete,2)==0;
+
+clean_T_dfa = T_dfa(idx_complete,:);
+
+
+  clear g;close all
+  g=gramm('x',clean_dat,'color',ind_RT);
+  % g.geom_jitter();
+  g.stat_bin('nbins',200,'dodge',0,'fill','transparent');
+  % g.stat_density()
+  % g.geom_abline()
+  g.set_names('column','Origin','x','Time (s)','color','Dists');
+  % g.set_text_options('base_size',20);
+  % g.set_color_options('chroma',0,'lightness',20)
+  % g(2,1)=gramm('x',RT_stimoff);
+  % % g.geom_jitter();
+  % g(2,1).stat_density();
+  % % g.geom_abline()
+  % g(2,1).set_names('column','Origin','x','Reaction time, stim');
+  g.set_text_options('base_size',20);
+  % g(2,1).set_color_options('chroma',0,'lightness',20)
+  g.set_title('Time (s) since rotation stop');
+  %g.facet_grid('space','free')
+  figure('Position',[100 100 800 600]);
+  g.draw();
+  g.facet_grid('scale','free')
+
+  cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/plots/')
+  %Name of figure
+  filetyp='svg';
+  %name filess
+  formatOut = 'yyyy-mm-dd';
+  todaystr = datestr(now,formatOut);
+  namefigure = sprintf('DFA_exponents');%fractionTrialsRemaining
+  filetype    = 'svg';
+  figurename = sprintf('%s_%s.%s',todaystr,namefigure,filetype);
+  g.export('file_name',figurename,'file_type',filetype);
+
 
 %load all the resting-state DFA
 
--- run_dfa.m
+++ run_dfa.m
@@ -1,86 +1,96 @@
 function run_dfa(cfgin)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Run Tom Pfeffer's implementation of DFA analysis, on restin
-%and trial based data
-%Created 2017-11-28
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %Run Tom Pfeffer's implementation of DFA analysis, on restin
+  %and trial based data
+  %Created 2017-11-28
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+  try
+    if strcmp(cfgin.blocktype,'resting')
+      %define ds file, this is actually from the trial-based data
+      if strcmp(cfgin.restingfile(1),'0')
+        dsfile = sprintf('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/%s/cleaned/P0%s_S%s_P%s.mat',cfgin.blocktype,cfgin.restingfile(2),cfgin.restingfile(5),cfgin.restingfile(8));
+        savefile= sprintf('P0%s_S%s_B%s.mat',cfgin.restingfile(2),cfgin.restingfile(5),cfgin.restingfile(8));
+      else
+        dsfile = sprintf('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/%s/cleaned/P%s_S%s_P%s.mat',cfgin.blocktype,cfgin.restingfile(1:2),cfgin.restingfile(5),cfgin.restingfile(8));
+        savefile= sprintf('P%s_S%s_B%s.mat',cfgin.restingfile(1:2),cfgin.restingfile(5),cfgin.restingfile(8));
+      end
+    elseif strcmp(cfgin.blocktype,'trial')
+      %load clean data
+      if strcmp(cfgin.restingfile(3),'_')
+        dsfile = sprintf('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/%s/cleaned/P%s_s%s_b%s.mat',cfgin.blocktype,cfgin.restingfile(2),cfgin.restingfile(5),cfgin.restingfile(8));
+        savefile= sprintf('P%s_s%s_b%s.mat',cfgin.restingfile(2),cfgin.restingfile(5),cfgin.restingfile(8));
+      else
+        dsfile = sprintf('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/%s/cleaned/P%s_s%s_b%s.mat',cfgin.blocktype,cfgin.restingfile(2:3),cfgin.restingfile(6),cfgin.restingfile(9));
+        savefile= sprintf('P%s_s%s_b%s.mat',cfgin.restingfile(2:3),cfgin.restingfile(6),cfgin.restingfile(9));
+      end
+    end
+    %P08_S2_P1.mat, P08_S3_P1.mat
+    load(dsfile)
+
+    %Define bandpass intervals
+    intervals = [2 4;4 8;8 12;12 24];
+
+    for i_val = 1:length(intervals)
+      disp(i_val)
+      %bandpass filter signal
+      cfg =[];
+      cfg.bpfreq = intervals(i_val,:);
+      cfg.bpfilttype='but';
+      cfg.channel = 'MEG';
+      dataBP=ft_preprocessing(cfg,data);
+
+      %compute the amplitude envelope
+      dataBP.trial{1}=abs(hilbert(dataBP.trial{1}));
+      %Compute DFA
+      % Uses the following inputs:
+      % x:        signal to be analyzed
+      % win:      length of fitting window in seconds (default: [1 50])
+      % Fs:       sampling rate
+      % overlap:  overlap of windows (default: 0.5)
+      % binnum:   number of time bins for fitting (default: 10)
+      if strcmp(cfgin.blocktype,'resting')
+        win = [3 30];
+      else
+        win = [3 50];
+      end
+      Fs  = 500;
+      overlap = 0.5;
+      binnum = 10;
+      dfa = tp_dfa(dataBP.trial{1}',win,Fs,overlap,binnum)
+
+      dfa_all{i_val}=dfa;
+
+    end
+
+    if strcmp(cfgin.blocktype,'resting')
+      cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/resting/DFA')
+    else
+      cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/trial/DFA')
+    end
+
+    save(savefile,'dfa_all')
 
 
-if strcmp(cfgin.blocktype,'resting')
-  %define ds file, this is actually from the trial-based data
-  if strcmp(cfgin.restingfile(1),'0')
-    dsfile = sprintf('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/%s/cleaned/P0%s_S%s_P%s.mat',cfgin.blocktype,cfgin.restingfile(2),cfgin.restingfile(5),cfgin.restingfile(8));
-    savefile= sprintf('P0%s_S%s_B%s.mat',cfgin.restingfile(2),cfgin.restingfile(5),cfgin.restingfile(8));
-  else
-    dsfile = sprintf('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/%s/cleaned/P%s_S%s_P%s.mat',cfgin.blocktype,cfgin.restingfile(1:2),cfgin.restingfile(5),cfgin.restingfile(8));
-    savefile= sprintf('P%s_S%s_B%s.mat',cfgin.restingfile(1:2),cfgin.restingfile(5),cfgin.restingfile(8));
+    %TODO: Check the number of components removed from each participant.
+    %TODO: Figure out best way of storage.
+    %TODO: Decide on loops, need to compute for each freq band. Maybe cluster is
+    %TODO: Append the data from same session
+    %TODO: Re-run all DFAs
+    %better after all.
+
+
+
+  catch err
+
+    cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/')
+    fid=fopen('logfile_dfa','a+');
+    c=clock;
+    fprintf(fid,sprintf('\n\n\n\nNew entry for %s at %i/%i/%i %i:%i\n\n\n\n',cfgin.restingfile,fix(c(1)),fix(c(2)),fix(c(3)),fix(c(4)),fix(c(5))))
+
+    fprintf(fid,'%s',err.getReport('extended','hyperlinks','off'))
+
+    fclose(fid)
+
   end
-elseif strcmp(cfgin.blocktype,'trial')
-  %load clean data
-  if strcmp(cfgin.restingfile(3),'_')
-    dsfile = sprintf('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/%s/cleaned/P%s_s%s_b%s.mat',cfgin.blocktype,cfgin.restingfile(2),cfgin.restingfile(5),cfgin.restingfile(8));
-    savefile= sprintf('P%s_s%s_b%s.mat',cfgin.restingfile(2),cfgin.restingfile(5),cfgin.restingfile(8));
-  else
-    dsfile = sprintf('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/%s/cleaned/P%s_s%s_b%s.mat',cfgin.blocktype,cfgin.restingfile(2:3),cfgin.restingfile(6),cfgin.restingfile(9));
-    savefile= sprintf('P%s_s%s_b%s.mat',cfgin.restingfile(2:3),cfgin.restingfile(6),cfgin.restingfile(9));
-  end
-end
-%P08_S2_P1.mat, P08_S3_P1.mat
-load(dsfile)
-
-%Define bandpass intervals
-intervals = [2 4;4 8;8 12;12 24];
-
-for i_val = 1:length(intervals)
-  disp(i_val)
-  %bandpass filter signal
-  cfg =[];
-  cfg.bpfreq = intervals(i_val,:);
-  cfg.bpfilttype='but';
-  cfg.channel = 'MEG';
-  dataBP=ft_preprocessing(cfg,data);
-
-  %compute the amplitude envelope
-  dataBP.trial{1}=abs(hilbert(dataBP.trial{1}));
-  %Compute DFA
-  % Uses the following inputs:
-  % x:        signal to be analyzed
-  % win:      length of fitting window in seconds (default: [1 50])
-  % Fs:       sampling rate
-  % overlap:  overlap of windows (default: 0.5)
-  % binnum:   number of time bins for fitting (default: 10)
-  if strcmp(cfgin.blocktype,'resting')
-    win = [3 30];
-  else
-    win = [3 50];
-  end
-  Fs  = 500;
-  overlap = 0.5;
-  binnum = 10;
-  dfa = tp_dfa(dataBP.trial{1}',win,Fs,overlap,binnum)
-
-  dfa_all{i_val}=dfa;
-
-end
-
-if strcmp(cfgin.blocktype,'resting')
-  cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/resting/DFA')
-else
-  cd('/mnt/homes/home024/chrisgahn/Documents/MATLAB/ktsetsos/trial/DFA')
-end
-
-save(savefile,'dfa_all')
-
-
-%TODO: Check the number of components removed from each participant.
-%TODO: Figure out best way of storage.
-%TODO: Decide on loops, need to compute for each freq band. Maybe cluster is
-%TODO: Append the data from same session
-%TODO: Re-run all DFAs
-%better after all.
-
-
-
-
-
 end
