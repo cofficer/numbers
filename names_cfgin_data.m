5adcf91cac6901e3c84d28c0006431c03667d119
cofficer <christoffer.gahnstrom@hotmail.com>
2017-12-21 21:00:44 +0100

    strange bug

--- names_cfgin_data.m
+++ names_cfgin_data.m
@@ -4,12 +4,14 @@ function cfgin=names_cfgin_data(blocktype)
 
   if strcmp(blocktype,'resting')
     cd('/home/ktsetsos/resting')
+    restingpaths  = dir('*S*.mat');
   elseif strcmp(blocktype,'trial')
     cd('/home/ktsetsos/preproc3')
+    restingpaths  = dir('*.mat');
   end
 
   %Sort the sessions and id in correct order.
-  restingpaths  = dir('*.mat');
+
   restingpaths  = {restingpaths.name};
   if strcmp(blocktype,'resting')
     sort_sessions = cellfun(@(x) x(1:2),restingpaths,'UniformOutput',false);
