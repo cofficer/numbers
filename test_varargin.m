c305e2bb5bfd90475b3a33fe54763ba0c08ee518
cofficer <christoffer.gahnstrom@hotmail.com>
2018-02-06 17:52:32 +0100

    -

--- test_varargin.m
+++ test_varargin.m
@@ -1,10 +1,9 @@
 function output=test_varargin(varargin)
 
 cfgin=varargin{1}
-data=varargin{2}
+data12=varargin{2}
 comp=varargin{3}
 
-
-output='test worked';
+output=length(data12.time{1});
 
 end
